a642cded36241520c999b2539b8c870b88658ef9
devel <develop.raul@gmail.com>
2022-11-21 23:33:12 +0900

    first commit

---
+++ components/Theme.js
@@ -0,0 +1,37 @@
+const Theme = {
+    sizes: {
+        fonts: {
+            regular: 10,
+            bigger: 14,
+            btn: 20,
+            btn_small: 14,
+            title_sub: 20,
+            title: 48,
+            tag: 14
+        }
+    },
+    colors: {
+        fonts: {
+            main: "#000000",
+            error: "#FF7171",
+            placeholder: "#BBBBBB",
+            btn_form: "#FFFFFF",
+            btn_cta: "#000000",
+        },
+        backgrounds: {
+            btn_form: "#000000",
+            btn_cta: "#FFFFFF",
+            select: "#FFFFFF",
+            select_hover: "F7F7F7",
+            input: "#FFFFFF"
+        },
+        strokes: {
+            btn_form: "#000000",
+            btn_cta: "#000000",
+            input: "#000000",
+            input_unwritable: "#BBBBBB"
+        }
+    }
+}
+
+export default Theme
