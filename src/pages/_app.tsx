45f130cd8ae8167bee72055f60de9aefef4e3ba2
devel <develop.raul@gmail.com>
2022-11-22 22:07:49 +0900

    fix: 패키지 구조 정리
    기존 style-component 관련 파일들을 src/theme 에서 style로 이전

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import '../../styles/global.css'
-import regularTheme from '../theme/theme'
-import GlobalStyle from '../theme/GlobalStyle'
+import regularTheme from '../../styles/theme'
+import GlobalStyle from '../../styles/GlobalStyle'
 
 const App = ({Component, pageProps}: AppProps) => {
     return (
