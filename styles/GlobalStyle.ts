f277ee65037f2e91457e6f3c7a879d356dd5639a
devel <develop.raul@gmail.com>
2022-11-24 13:53:02 +0900

    feat: global style 추가
    a태그 관련 default style 제거

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,6 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+    a {
+        color: #fff;
+        text-decoration: none;
+        outline: none;
+    }
+
+    a:hover, a:active {
+        text-decoration: none;
+        color:#fff;
+    }
+
     body {
         font-family: 'Pretendard-Regular'
     }
