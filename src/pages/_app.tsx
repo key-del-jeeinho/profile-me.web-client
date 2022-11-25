5e6475e0691f4de2c8c2ca164103dad85992dcef
devel <develop.raul@gmail.com>
2022-11-25 23:15:13 +0900

    feat: recoil 도입

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,14 @@ import { AppProps } from 'next/app'
 import '../../styles/global.css'
 import "../../public/fonts/style.css"
 import withTheme from '../../styles/withTheme'
+import { RecoilRoot } from 'recoil'
 
 const App = ({Component, pageProps}: AppProps) => {
-    return <>{withTheme(() => <Component {...pageProps}/>)}</>
+    return <>{withTheme(() => (
+        <RecoilRoot>
+            <Component {...pageProps}/>
+        </RecoilRoot>
+    ))}</>
 }
 
 export default App
